ae306bfe37ca6a150352a15f77d9386a9115cc3d
Defiance Black <parley@portroyal.ltd>
2021-10-26 07:06:52 -0400

    use MaryMaid's roll instead

--- src/twitch/commands/roll.js
+++ src/twitch/commands/roll.js
@@ -1,6 +1,23 @@
 const {DiceRoll} = require("rpg-dice-roller")
 
 module.exports = async (client, target, context, message, args) => {
-  let roll = new DiceRoll("1d%")
+  let roll
+  let d = args[0] === "d"
+
+  if (d) {
+    roll = new DiceRoll(args[1] || "1d%")
+    client.say(target, `@${context["display-name"]} rolled ${roll.output}.`)
+
+    return
+  }
+
+  let num = args[0] ? Number(args[0]) : undefined
+
+  if (args?.length === 1 && num) {
+    roll = new DiceRoll(`1d${num}`)
+  } else {
+    roll = new DiceRoll("1d%")
+  }
+  
   client.say(target, `@${context["display-name"]} rolled ${roll.total}.`)
 }
--- src/twitch/commands/rolld.js
+++
@@ -1,6 +0,0 @@
-const {DiceRoll} = require("rpg-dice-roller")
-
-module.exports = async (client, target, context, message, args) => {
-  let roll = new DiceRoll(args[0] || "1d%")
-  client.say(target, `@${context["display-name"]} rolled ${roll.output}.`)
-}
