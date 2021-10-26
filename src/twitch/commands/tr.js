120f30edbefc164bf70c9d4f9fadf36a80dcb06e
Defiance Black <parley@portroyal.ltd>
2021-10-26 06:57:00 -0400

    Tu es incroyable!

--- src/twitch/commands/tr.js
+++ src/twitch/commands/tr.js
@@ -1,23 +1,61 @@
-const GT = require("@iamtraction/google-translate")
-require("dotenv").config()
+const ax = require("axios")
+const gt = require("@iamtraction/google-translate")
 
 const PS2 = process.env.TWITCH_PREFIX
 
 module.exports = async (client, target, context, message, args) => {
   // #'tr [-f auto, -t en] Tu es incroyable!
-  let fromLang = "auto"
-  let toLang = "en"
-  let help = () => client.say(target, `${PS2}tr Tu es incroyable!`)
-
-  let args0 = args?.[0]?.toLowerCase()
-  let args1 = args?.[1]?.toLowerCase()
-  let args2 = args?.[2]?.toLowerCase()
-  
-  let gtOpts = {
-    from: fromLang,
-    to: toLang,
-    raw: false
+  let help = () => {
+    let res = `@${context["display-name"]}, try, "${PS2}tr Tu es incroyable!" without quotes.`
+    res = res.concat(` ${PS2}tr also accepts -f XX and -t XX where XX is a two letter language code.`)
+    
+    client.say(target, res)
   }
 
-  client.say(target, `Respond to ${args} or parse ${message} directly.`)
+  let fromLang = "auto"
+  let fromAliases = ["-f", "-fr", "-fro", "-from"]
+  let toLang = "en"
+  let toAliases = ["-t", "-to"]
+  let trText = ""
+
+  let gtOpts = () => {
+    return {
+      from: fromLang,
+      to: toLang,
+      raw: false
+    }
+  }
+  let translate = (words) => {
+    gt(words, gtOpts())
+      .then((res) => {
+        let fR = res.from.language.iso.toUpperCase()
+        let tO = gtOpts().to.toUpperCase()
+
+        client.say(target, `@${context["display-name"]} [${fR}->${tO}] ${res.text}`)
+      })
+      .catch((error) => {
+        let res = `[${PS2}tr]: @${context["display-name"]}, there was an error in your request.`
+        res = res.concat(" Note that language codes to -f (from) or -t (to) are only two letters.")
+        
+        client.say(target, res)
+      })
+  }
+
+  if (!args.length || args[0] === "help") {
+    help()
+    return
+  }
+  
+  if (fromAliases.indexOf(args[0]) !== -1) { // -f ?
+    args.shift() // toss -f
+    fromLang = args.shift() 
+  }
+
+  if (toAliases.indexOf(args[0]) !== -1) { // -t ?
+    args.shift() // toss -t
+    toLang = args.shift()
+  }
+
+  trText = args.join(" ")
+  translate(trText)
 }
